749e394e8ff61a04d29de4914c4bae015d9d9ac9
Myssenberg <my.nordaljensen@gmail.com>
2021-10-15 14:50:42 +0200

    Tests done for spaghetti code

--- GildedRose.Tests/TestAssemblyTests.cs
+++ GildedRose.Tests/TestAssemblyTests.cs
@@ -1,50 +1,38 @@
-﻿using System.Collections.Generic;
-using GildedRose.Console;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using Xunit;
+﻿using Xunit;
 
 namespace GildedRose.Tests
 {
     public class TestAssemblyTests
     {
         private GildedRose.Console.Program _program;
+
         public TestAssemblyTests()
         {
             _program = new GildedRose.Console.Program();
-            
         }
-        [Fact]
-        public void Brie_going_from_2_to_1_days_left_should_increase_quality_by_two()
-        {
-            // Arrange
-            GildedRose.Console.Program p = new GildedRose.Console.Program();
 
-            // Act
-            p.UpdateQuality();
-
-            // Assert
-            Assert.Equal(1, p.Items[1].Quality);
-            Assert.Equal(1, p.Items[1].SellIn);
-        }
-        
         [Theory]
-        [InlineData(0, 100, -90, )]
-        [InlineData(0, 0, , )]
-        [InlineData(0, 1, , )]
+        [InlineData(0, 100, -90, 0)]
+        [InlineData(0, 0, 10, 20)]
+        [InlineData(0, 1, 9, 19)]
+        [InlineData(0, 12, -2, 6)]
         [InlineData(2, 100, -95, 0)]
         [InlineData(2, 0, 5, 7)]
         [InlineData(2, 1, 4, 6)]
-        public void Generic_item_aging_progresses_correctly(int itemIndex, int iterations, int expectedSellIn, int expectedQuality)
+        [InlineData(2, 5, 0, 2)]
+        [InlineData(2, 6, -1, 0)]
+        public void Generic_item_aging_progresses_correctly(int itemIndex, int iterations, int expectedSellIn,
+            int expectedQuality)
         {
             //Act
             for (int i = 0; i < iterations; i++)
             {
                 _program.UpdateQuality();
             }
-            
+
             //Assert
             Assert.Equal(expectedSellIn, _program.Items[itemIndex].SellIn);
-            Assert.Equal(expectedSellIn, _program.Items[itemIndex].Quality);
+            Assert.Equal(expectedQuality, _program.Items[itemIndex].Quality);
         }
 
         [Theory]
@@ -64,44 +52,42 @@ namespace GildedRose.Tests
             Assert.Equal(expectedQuality, _program.Items[1].Quality);
         }
 
-        
+
         [Theory]
         [InlineData(0, 0, 80)]
         [InlineData(1, 0, 80)]
-        public void Sulfuras_should_not_age_nor_decrease_in_quality(int iterations, int expectedSellIn, int expectedQuality)
+        [InlineData(100, 0, 80)]
+        [InlineData(1000, 0, 80)]
+        public void Sulfuras_should_not_age_nor_decrease_in_quality(int iterations, int expectedSellIn,
+            int expectedQuality)
         {
             //Act
             for (int i = 0; i < iterations; i++)
             {
                 _program.UpdateQuality();
             }
-                
+
             //Assert
-            Assert.Equal(expectedSellIn, _program.Items[5].SellIn);
-            Assert.Equal(expectedQuality, _program.Items[5].Quality);
+            Assert.Equal(expectedSellIn, _program.Items[3].SellIn);
+            Assert.Equal(expectedQuality, _program.Items[3].Quality);
         }
-        
-        
-        
-        
-        
+
         [Theory]
-        [InlineData(0, 15, 20] //Original item
-        [InlineData(0, 15, 20] //"Quality increases by 2 when there are 10 days or less
-        public void Backstage_passes_quality_updates_correctly(int iterations, int expectedSellIn, int expectedQUality){
+        [InlineData(0, 15, 20)] //Original item
+        [InlineData(1, 14, 21)] //Quality increases
+        [InlineData(6, 9, 27)] //"Quality increases by 2 when there are 10 days or less
+        [InlineData(11, 4, 38)] //"Quality increases by 3 when there are 5 days or less
+        public void Backstage_passes_quality_updates_correctly(int iterations, int expectedSellIn, int expectedQUality)
+        {
             //Act
             for (int i = 0; i < iterations; i++)
-                    {
-                        _program.UpdateQuality();
-                     }
-                
-            //Assert
-            Assert.Equal(expectedSellIn, _program.Items[5].SellIn);
-            Assert.Equal(expectedQUality, _program.Items[5].Quality);
-        
-        
-        }
-        
+            {
+                _program.UpdateQuality();
+            }
 
+            //Assert
+            Assert.Equal(expectedSellIn, _program.Items[4].SellIn);
+            Assert.Equal(expectedQUality, _program.Items[4].Quality);
+        }
     }
 }
